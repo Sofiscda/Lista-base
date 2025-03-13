c79930c3e24324e815c481cce2d4b167df87c3f3
Sofiscda <165416068+Sofiscda@users.noreply.github.com>
2025-03-13 12:00:35 -0300

    Add files via upload

---
+++ lista base.py
@@ -0,0 +1,12 @@
+lista=['sofia','flavia','pedro','victor','ale','victoria','felipe','ane']
+#adicionar nomes
+lista.append('joão')
+#tbm serve pra inserir um nome
+lista.insert(1,'3dam')
+#deletar algo da lista
+del(lista[7])
+#ordenaros elementos da lista
+lista.sort()
+for nome in lista:
+    print(nome)
+#fim
